19abd7e8a5fdfccb3a1b7e0845a5511ae6f8d581
gvincennie <48695215+gvincennie@users.noreply.github.com>
2019-04-23 12:57:34 -0500

    Create wptp-footer.php

---
+++ wptp-footer.php
@@ -0,0 +1,3 @@
+<div class="wptp-footer">
+	<a href="https://linksoftwarellc.com"><?php _e('A WordPress Plugin by', WP_TERMS_POPUP_NAME); ?> <img src="<?php echo plugins_url('/assets/img/linksoftware.png', dirname(__FILE__)); ?>" alt="Link Software LLC" /> Link Software LLC</a>
+</div>
